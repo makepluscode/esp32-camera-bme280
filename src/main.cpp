c03641a2c72747199ea28ed6abc8b571f5a47f60
makepluscode <makepluscode@gmail.com>
2019-04-21 19:21:06 +0900

    Disable ENABLE_BME280 for my board which doesn't have BME280.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 
 #define ENABLE_SSD1306
 #define SOFTAP_MODE       //The comment will be connected to the specified ssid
-#define ENABLE_BME280
+//#define ENABLE_BME280
 
 #define WIFI_SSID   "your wifi ssid"
 #define WIFI_PASSWD "you wifi password"
